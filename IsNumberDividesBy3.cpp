ee0bd9116683216867927c8272dcdf4a2c521ec0
Alexander <pchp250g8@tutamail.com>
2026-04-06 17:40:14 +0300

    Created Project IsNumberDividesBy3

---
+++ IsNumberDividesBy3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <tchar.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    int iNum, iVal, iSumDigits;
+    system("cls");
+    std::cout << "Input an integer number\r\n";
+    std::cin >> iNum;
+    iVal = abs(iNum); iSumDigits = 0;
+    while (iVal > 0)
+    {
+        iSumDigits += iVal % 10;
+        iVal /= 10;
+    }
+    std::cout << "The sum of digits of the number " << iNum << " is "
+    		  << iSumDigits << "\r\n";
+    if (iSumDigits % 3 == 0)
+    {
+        std::cout << "The number " << iNum << " divides by 3\r\n";
+    }
+    else
+    {
+        std::cout << "The number " << iNum << " does not divide by 3\r\n";
+    }
+    std::cin.ignore();
+    std::cin.get();
+    return 0;
+}
